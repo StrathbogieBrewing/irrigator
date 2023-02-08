5b0518c5d00bf65ce9d8dfd44f143ea1e171b5ca
JohnyHoops <johnyhoops@tinbus.org>
2023-02-08 15:20:34 +1100

    deployed

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <util/delay.h>
 
-// #include "trace.h"
+#include "trace.h"
 
 /* AVR GPIO helper macros */
 // clang-format off
@@ -16,7 +16,6 @@
 #define GPIO_GET(pin) (GPIO_GET_BIT(PIN, pin))
 // clang-format on
 
-
 #define VALVE_0 B, 0
 #define VALVE_1 B, 1
 #define VALVE_2 D, 2
@@ -27,6 +26,8 @@
 #define VALVE_7 D, 7
 
 #define ALL_VALVES_OFF 8
+
+#define VBAT_ADC_CHANNEL 0
 
 // clang-format off
 static const uint8_t slots[16] = {
@@ -70,32 +71,76 @@ void activate_valve(uint8_t valve) {
     GPIO_SET(VALVE_7, valve == 7);
 }
 
+/* AVR adc helper function*/
+// set ADC prescaler for 50-200 KHz
+#if F_CPU >= 16000000 // 16 MHz / 128 = 125 KHz
+#define ADC_ADPS 7
+#elif F_CPU >= 8000000 // 8 MHz / 64 = 125 KHz
+#define ADC_ADPS 6
+#elif F_CPU >= 4000000 // 4 MHz / 32 = 125 KHz
+#define ADC_ADPS 5
+#elif F_CPU >= 2000000 // 2 MHz / 16 = 125 KHz
+#define ADC_ADPS 4
+#elif F_CPU >= 1000000 // 1 MHz / 8 = 125 KHz
+#define ADC_ADPS 3
+#else // 128 kHz / 2 = 64 KHz -> This is the closest you can get, the prescaler is 2
+#define ADC_ADPS 1
+#endif
+
+uint16_t adc_get(uint8_t channel) {
+    ADCSRA = (1 << ADEN) | ADC_ADPS;
+    ADMUX = (1 << REFS0) | (channel & 0x07);
+    ADCSRA |= (1 << ADSC);
+    while (ADCSRA & (1 << ADSC))
+        ;
+    return ADC;
+}
+
 int main(void) {
     uint8_t minute = 0;
+    uint32_t vbat_filter = 0;
+    uint32_t vbat_mv = 0;
 
     // trace_init();
     activate_valve(ALL_VALVES_OFF);
-    
+
     while (1) {
 
         // 1 minute (60 seconds) delay between minute changes
-        for (uint16_t i = 0; i < (60 * 100); i++) {
-            _delay_ms(10);
+        for (uint16_t i = 0; i < 60; i++) {
+
+            for (uint16_t i = 0; i < 100; i++) {
+                _delay_ms(10); // 1 second delay
+            }
+
+            vbat_filter -= vbat_filter >> 4UL;
+            vbat_filter += (uint32_t)adc_get(VBAT_ADC_CHANNEL);
+            vbat_mv = ((vbat_filter >> 4UL) * 7104L) >> 8;
+
+            // trace_write((uint8_t[]){(uint8_t)(vbat_mv / 100), (uint8_t)(vbat_mv % 100)}, 2);
+            // vbat_mv = ((uint32_t)adc_get(VBAT_ADC_CHANNEL) * 7104UL) / 256UL;
+            // if (vbat_mv > 13400UL) {
+            //     activate_valve(0);
+            // } else {
+            //     activate_valve(1);
+            // }
+        }
+
+        if (vbat_mv > 13400UL) {
+            minute += 1;
+        } else {
+            minute = 0;
         }
 
         uint8_t slot = (minute >> 4);
         uint8_t valve = (minute >> 1) & 0x07;
 
-        if(slots[slot] & (1 << valve)) {
+        if (slots[slot] & (1 << valve)) {
             activate_valve(valve);
 
         } else {
             activate_valve(ALL_VALVES_OFF);
         }
-
-        minute += 1;
-
-        // trace_write((uint8_t[]){seconds, minute}, 2);
     }
     return 0;
 }
